2f14878e26ecb12056d1f962a8256b158f5f933a
Anjorin Israel <israelwhiz@gmail.com>
2020-09-05 23:29:21 +0100

    all post now come from the database

--- single.php
+++ single.php
@@ -1,16 +1,26 @@
-<?php require_once "includes/header.php" ?>
+<?php require_once "includes/header.php" ;
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM posts WHERE id = :id";
+            $details = query($sql,array(
+                  'id'=>$id))->fetch(PDO::FETCH_OBJ);
+
+  $sql2 = "SELECT * FROM post_gallery WHERE post_id = :id";
+  $result = query($sql2,array(
+        'id'=>$id))->fetch(PDO::FETCH_OBJ)
+?>
     
     
-    <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('images/img1.jpg');">
+    <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('uploads/products<?php echo $result->path ?>');">
       <div class="container">
         <div class="row same-height justify-content-center">
           <div class="col-md-12 col-lg-10">
             <div class="post-entry text-center">
              
-              <h1 class="mb-4"><a href="#">Understanding SQL and writing queries</a></h1>
+              <h1 class="mb-4"><a href="#"><?php echo $details->title?></a></h1>
               <div class="post-meta align-items-center text-center"> 
-                <span class="d-inline-block mt-1">By Anjorin Israel</span>
-                <span>&nbsp;-&nbsp; February 10, 2019</span>
+                <span class="d-inline-block mt-1">By <?php echo $details->author?></span>
+                <span>&nbsp;-&nbsp; <?php echo $details->date?></span>
               </div>
             </div>
           </div>
@@ -26,15 +36,9 @@
           <div class="col-md-12 col-lg-12 main-content">
             
             <div class="post-content-body">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium nam quas inventore, ut iure iste modi eos adipisci ad ea itaque labore earum autem nobis et numquam, minima eius. Nam eius, non unde ut aut sunt eveniet rerum repellendus porro.</p>
-            <p>Sint ab voluptates itaque, ipsum porro qui obcaecati cumque quas sit vel. Voluptatum provident id quis quo. Eveniet maiores perferendis officia veniam est laborum, expedita fuga doloribus natus repellendus dolorem ab similique sint eius cupiditate necessitatibus, magni nesciunt ex eos.</p>
-            <p>Quis eius aspernatur, eaque culpa cumque reiciendis, nobis at earum assumenda similique ut? Aperiam vel aut, ex exercitationem eos consequuntur eaque culpa totam, deserunt, aspernatur quae eveniet hic provident ullam tempora error repudiandae sapiente illum rerum itaque voluptatem. Commodi, sequi.</p>
-            <p>Quibusdam autem, quas molestias recusandae aperiam molestiae modi qui ipsam vel. Placeat tenetur veritatis tempore quos impedit dicta, error autem, quae sint inventore ipsa quidem. Quo voluptate quisquam reiciendis, minus, animi minima eum officia doloremque repellat eos, odio doloribus cum.</p>
-            <p>Temporibus quo dolore veritatis doloribus delectus dolores perspiciatis recusandae ducimus, nisi quod, incidunt ut quaerat, magnam cupiditate. Aut, laboriosam magnam, nobis dolore fugiat impedit necessitatibus nisi cupiditate, quas repellat itaque molestias sit libero voluptas eveniet omnis illo ullam dolorem minima.</p>
-            <p>Porro amet accusantium libero fugit totam, deserunt ipsa, dolorem, vero expedita illo similique saepe nisi deleniti. Cumque, laboriosam, porro! Facilis voluptatem sequi nulla quidem, provident eius quos pariatur maxime sapiente illo nostrum quibusdam aliquid fugiat! Earum quod fuga id officia.</p>
-            <p>Illo magnam at dolore ad enim fugiat ut maxime facilis autem, nulla cumque quis commodi eos nisi unde soluta, ipsa eius aspernatur sint atque! Nihil, eveniet illo ea, mollitia fuga accusamus dolor dolorem perspiciatis rerum hic, consectetur error rem aspernatur!</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus magni explicabo id molestiae, minima quas assumenda consectetur, nobis neque rem, incidunt quam tempore perferendis provident obcaecati sapiente, animi vel expedita omnis quae ipsa! Obcaecati eligendi sed odio labore vero reiciendis facere accusamus molestias eaque impedit, consequuntur quae fuga vitae fugit?</p>
+            <p>
+            <?php echo $details->content?>
+            </p>
             </div>
 
            
