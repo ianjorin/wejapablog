41e00646ccadca7a968a1d4fb64c8950040ed60e
Anjorin Israel <israelwhiz@gmail.com>
2020-09-16 13:37:26 +0100

    added functionality for forgot password

---
+++ processors/new-pass.php
@@ -0,0 +1,12 @@
+<?php 
+
+ob_start();
+session_start();
+require_once('../config/constants.php');
+require_once('../functions.php');
+require_once('../classes/picture.class.php');
+require_once('../pdo_connection.php');
+require_once('../classes/user.class.php');
+
+$user = New User;
+$reset = $user->newpassword();
